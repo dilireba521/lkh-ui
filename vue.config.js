8dbdf25595708873ef48e802c383f8a58c93c6fa
hanlikui <likui.han@goqomo.com>
2022-02-11 17:27:52 +0800

    新增关联oss功能

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,30 @@
 var path = require("path");
-
+const WebpackAliyunOss = require('webpack-aliyun-oss');//上传到oss才需要引入
+const oss = require('./oss.config');
 module.exports = {
   pages: {
     index: {
       entry: 'examples/main.js',
       template: 'public/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
     }
   },
 
   configureWebpack: (config) => {
     //自动扩展文件后缀名，意味着我们require模块可以省略不写后缀名
     config.resolve.extensions = ['.js', '.ts', '.tsx', '.json', '.vue', '.css', '.scss'];
-
+    let webpackAliyunOss = [
+      new WebpackAliyunOss({
+        from: ['./dist/**'], // 上传那个文件或文件夹  可以是字符串或数组
+        // dist: "/test",  // 需要上传到oss上的给定文件目录
+        region: oss.region, // 只是示例，如果是别的地区请填别的地区
+        accessKeyId: oss.accessKeyId, // 不知道ak和sk的百度以下把～
+        accessKeySecret: oss.accessKeySecret,
+        bucket: oss.bucket,  // bucket的name
+        overwrite: true,//覆盖oss同名文件，必须为true否则再次上传时会报错
+      })
+    ]
+    config.plugins = [...config.plugins, ...webpackAliyunOss]
   },
   //扩展 webpack 配置，使packages加入编译
   chainWebpack: config => {
